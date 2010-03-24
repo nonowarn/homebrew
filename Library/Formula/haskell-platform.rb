c64c35a6ba6b0e6e60bdfea77ad21698cb8cebcc
nonowarn <nonowarn@gmail.com>
2010-03-25 06:45:48 +0900

    New fomula: haskell-platform
    The standard Haskell development environment

---
+++ Library/Formula/haskell-platform.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class HaskellPlatform <Formula
+  url 'http://hackage.haskell.org/platform/2010.1.0.0/haskell-platform-2010.1.0.0.tar.gz'
+  homepage 'http://hackage.haskell.org/platform/'
+  md5 'ad96a12535b955f2f5e11769b02e0f76'
+  version '2010.1.0.0'
+
+  depends_on 'ghc'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make install"
+
+    puts 'Installation completed successfully. Now run "cabal update" to initialize the package list'
+  end
+end
